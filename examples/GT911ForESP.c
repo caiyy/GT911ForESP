d96cd19ef6317fb2db7275b13ec3c9be108bf395
CodingSoul <codingsoul@CodingSuldeMini.lan>
2026-03-01 17:07:52 +0800

    更新示例文件
    - 修改头文件引用为 Vernon_GT911.h
    - 修改结构体名称为 Vernon_GT911
    - 使用 pdMS_TO_TICKS 宏替代 portTICK_PERIOD_MS
    - 添加中文注释

--- examples/GT911ForESP.c
+++ examples/GT911ForESP.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-
-#include "Alfred_GT911.h"
+#include "Vernon_GT911.h"
 
 #define TOUCH_GT911_SDA 41
 #define TOUCH_GT911_SCL 42
@@ -9,30 +8,32 @@
 #define SCREEN_WIDTH 480
 #define SCREEN_HEIGHT 480
 
-Alfred_GT911 alfredGt911;
+static Vernon_GT911 gt911;
 
 void GT911_test(void * pvPara)
 {
-    int x, y;
+    uint16_t x, y;
     while(1){
-        if(GT911_touched(&alfredGt911)){
-            //循环读取每个触控点的位置值
+        if(GT911_touched(&gt911)){
+            // 循环读取每个触控点的位置值
             for (int i = 0; i < 5; ++i) {
-                GT911_read_pos(&alfredGt911, &x, &y, i);
-                printf("No: %d, touched x: %d, touched y: %d\n", i,  x, y);
+                GT911_read_pos(&gt911, &x, &y, i);
+                printf("No: %d, touched x: %d, touched y: %d\n", i, x, y);
             }
         }
-        vTaskDelay(100 / portTICK_PERIOD_MS);
+        vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
 
 void app_main(void)
 {
-    GT911_init(&alfredGt911, TOUCH_GT911_SDA, TOUCH_GT911_SCL, TOUCH_GT911_INT,
-               TOUCH_GT911_RES, I2C_NUM_0,GT911_ADDR1,
+    // 初始化 GT911
+    GT911_init(&gt911, TOUCH_GT911_SDA, TOUCH_GT911_SCL, TOUCH_GT911_INT,
+               TOUCH_GT911_RES, I2C_NUM_0, GT911_ADDR1,
                SCREEN_WIDTH, SCREEN_HEIGHT);
-    //初始化之后必须设置
-    GT911_setRotation(&alfredGt911, ROTATION_NORMAL);
+    
+    // 初始化之后必须设置旋转方向
+    GT911_setRotation(&gt911, ROTATION_NORMAL);
 
     xTaskCreate(GT911_test, "GT911_test", 1024 * 3,
                 NULL, 12, NULL);
